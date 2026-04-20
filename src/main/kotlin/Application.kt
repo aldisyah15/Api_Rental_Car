1ae5046e8aae9f90e3b79cb1e119973785a106a7
aldisyah15 <aldisyah687@gmail.com>
2026-04-20 08:16:17 +0700

    add : CORS

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,15 +1,19 @@
 package com.example
 
 import com.example.routing.configureRouting
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-import kotlinx.serialization.json.Json
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.plugins.cors.routing.*
 
 fun main(args: Array<String>) {
+
+    val port = System.getenv("PORT")?.toInt() ?: 8080
+
     io.ktor.server.netty.EngineMain.main(args)
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+    embeddedServer(Netty, port = port, host = "0.0.0.0") {
         install(io.ktor.server.plugins.contentnegotiation.ContentNegotiation) {
             json()
         }
@@ -18,7 +22,13 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    install(CORS) {
+        anyHost()
+        allowMethod(HttpMethod.Get)
+        allowMethod(HttpMethod.Post)
+        allowHeader(HttpHeaders.ContentType)
+    }
     configureSerialization()
-    configureHTTP()
+
     configureRouting()
 }
