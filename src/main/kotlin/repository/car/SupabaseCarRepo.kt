55ed13fb552ae27b017c907dca7c4f4ed6aa0c0c
aldisyah15 <aldisyah687@gmail.com>
2026-04-24 18:52:55 +0700

    add : method get_brands

---
+++ src/main/kotlin/repository/car/SupabaseCarRepo.kt
@@ -0,0 +1,14 @@
+package com.example.repository.car
+
+import com.example.database.supabase
+import com.example.model.Brand_car
+import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.storage.storage
+
+class SupabaseCarRepo() {
+    suspend fun get_brands(): List<Brand_car> {
+        val bucket = supabase.from("car").select().decodeList<Brand_car>()
+        return bucket
+
+    }
+}
