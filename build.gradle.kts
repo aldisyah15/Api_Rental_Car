1e72662b09d4ca56bbbeb1208ae4a2cf42391923
aldisyah15 <aldisyah687@gmail.com>
2026-04-24 18:50:52 +0700

    add : depedency supabase:storage

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
     implementation("io.github.jan-tennert.supabase:auth-kt")
     implementation("io.github.jan-tennert.supabase:realtime-kt")
+    implementation("io.github.jan-tennert.supabase:storage-kt")
 
     implementation("io.ktor:ktor-server-auth-jwt:${kotlin_version}")
     implementation("io.ktor:ktor-client-cio:3.4.2")
