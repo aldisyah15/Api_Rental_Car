b66bcfe27d83ab4b133d35800058b827f927bd1c
aldisyah15 <aldisyah687@gmail.com>
2026-04-26 06:44:41 +0700

    Add : data class CarRespon

---
+++ src/main/kotlin/model/CarRespond.kt
@@ -0,0 +1,20 @@
+package com.example.model
+
+import kotlinx.serialization.Serializable
+
+data class CarRespond(
+    val brand_name: String,
+    val url_logo: String,
+    val rental_price: String,
+    val horse_power: String,
+    val transmission: String,
+    val vehicle_photo: String,
+    val detail: String,
+    val sales: SalesProfile
+)
+
+data class SalesProfile(
+    val sales_name : String,
+    val sales_photo: String,
+    val contact_number_whatsapp: String
+)
