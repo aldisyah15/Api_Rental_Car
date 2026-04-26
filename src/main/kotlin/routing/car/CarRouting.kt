1b71b17e30c09d056b2cd686fc0529a0def26257
aldisyah15 <aldisyah687@gmail.com>
2026-04-26 07:21:07 +0700

    Add : getCars

--- src/main/kotlin/routing/car/CarRouting.kt
+++ src/main/kotlin/routing/car/CarRouting.kt
@@ -1,7 +1,9 @@
 package com.example.routing.car
 
+import com.example.model.ApiRespondCar
 import com.example.model.Brand_car
 import com.example.repository.car.SupabaseCarRepo
+import io.ktor.http.HttpStatusCode
 import io.ktor.server.auth.authenticate
 import io.ktor.server.auth.jwt.JWTPrincipal
 import io.ktor.server.auth.principal
@@ -12,14 +14,33 @@ import io.ktor.server.routing.post
 import io.ktor.server.routing.route
 
 fun Route.CarRouting() {
-authenticate("jwt-auth") {
-    route("/car") {
-        get("/brands") {
-            val res = SupabaseCarRepo()
-            call.respond(
-                res.get_brands()
-            )
+    authenticate("jwt-auth") {
+        route("/cars") {
+            get("/") {
+                val res = SupabaseCarRepo()
+
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    ApiRespondCar(
+                        succes = true,
+                        data = res.getCars()
+                    )
+                )
+
+            }
+
+            get("/brands") {
+                val res = SupabaseCarRepo()
+                call.respond(
+                    status = HttpStatusCode.OK,
+                    ApiRespondCar(
+                        succes = true,
+                        data = res.get_brands()
+                    )
+                )
+            }
+
+
         }
     }
-}
 }
