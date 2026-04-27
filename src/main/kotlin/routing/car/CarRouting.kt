9c80a852f977d53ca280b4b598eed83b77acd147
aldisyah15 <aldisyah687@gmail.com>
2026-04-27 23:16:57 +0700

    add : multipart

--- src/main/kotlin/routing/car/CarRouting.kt
+++ src/main/kotlin/routing/car/CarRouting.kt
@@ -17,6 +17,7 @@ import io.ktor.server.routing.Route
 import io.ktor.server.routing.get
 import io.ktor.server.routing.post
 import io.ktor.server.routing.route
+import io.ktor.util.valuesOf
 import io.ktor.utils.io.readByte
 import io.ktor.utils.io.readByteArray
 
@@ -28,23 +29,24 @@ fun Route.CarRouting() {
                 val res = SupabaseCarRepo()
                 var multipart = call.receiveMultipart()
                 var brand_name = ""
-                var url_logo = ""
                 var rental_price = ""
                 var horse_power = ""
                 var transmission = ""
-                var vehicle_photo = ""
                 var detail = ""
                 var sales_name = ""
-                var sales_photo = ""
                 var contact_number_whatsapp = ""
-                var fileBytes: ByteArray? = null
-                var fileName = ""
+                var url_logo: ByteArray? = null
+                var fileName_url_logo = ""
 
+                var sales_photo: ByteArray? = null
+                var fileName_sales_photo = ""
+
+                var vehicle_photo: ByteArray? = null
+                var fileName_vehicle_photo = ""
                 multipart.forEachPart { part ->
                     when (part) {
                         is PartData.FormItem -> {
                             if (part.name == "brand_name") brand_name = part.value
-                            if (part.name == "url_logo") url_logo = part.value
                             if (part.name == "rental_price") rental_price = part.value
                             if (part.name == "horse_power") horse_power = part.value
                             if (part.name == "transmission") transmission = part.value
@@ -53,26 +55,29 @@ fun Route.CarRouting() {
                             if (part.name == "sales_photo") sales_photo = part.value
                             if (part.name == "contact_number_whatsapp") contact_number_whatsapp = part.value
                         }
+
                         is PartData.FileItem -> {
-                            fileName = "${System.currentTimeMillis()}-${part.originalFileName}"
-                            fileBytes = part.streamProvider().readBytes()
+                            if (part.name == "url_logo") url_logo = part
+                            val fileName = "${System.currentTimeMillis()}-${part.originalFileName}"
+                            val fileBytes = part.streamProvider().readBytes()
                         }
+
                         else -> part.dispose
                     }
                 }
 
-                if (fileBytes != null) {
+                if (url_logo != null || sales_photo != null || vehicle_photo != null) {
                     val carObject = CarRespond(
                         brand_name = brand_name,
-                        url_logo = url_logo,
+                        url_logo = fileName_url_logo, //url
                         rental_price = rental_price.toInt(),
                         contact_number_whatsapp = contact_number_whatsapp,
                         detail = detail,
                         horse_power = horse_power,
                         sales_name = sales_name,
-                        sales_photo = sales_photo,
+                        sales_photo = fileName_sales_photo, //url
                         transmission = transmission,
-                        vehicle_photo = vehicle_photo,
+                        vehicle_photo = fileName_vehicle_photo, // url
                         id_car = null
                     )
                     res.postCar(fileName = fileName, filebytes = fileBytes, newCar = carObject)
